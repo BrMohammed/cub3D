5dd4dfcbb098c12a833a098a772d638cb8596237
mohammed <togachivon@gmail.com>
2022-07-23 20:48:44 +0100

    almost_end_of_y_ray01

--- src/ray_colesion.c
+++ src/ray_colesion.c
@@ -1,15 +1,56 @@
 #include "../Includes/cub.h"
 
+int y_detect(t_data *data, int y,int up_down)
+{
+    int x;
+
+    x = 0;
+    int a[2];
+	int b[2];
+    double tpx =0;
+    double tpy = 0;
+    while (data->result[y][x])
+    {
+        if(data->result[y][x] == '1') //wall detect posetion
+        {
+           
+            if (up_down == 1) 
+            {
+                tpy = ((data->player_y) - (((y + 1) * 50))) + 7.5;
+                tpx = tan(data->pa) * tpy;
+            }
+            else
+            {
+                tpy = ((((y) * 50)) - (data->player_y)) + 7.5;
+                tpx = -tan(data->pa) * tpy;
+            }
+            a[0] = data->player_x + 7.5;
+            a[1] = data->player_y + 7.5;
+            b[0] =   tpx + data->player_x;
+            if (up_down == 1) 
+                b[1] = (((y + 1) * 50));
+            else
+               b[1] = (((y) * 50));
+            if(b[0] >= x * 50 && b[0] <= (x + 1) * 50)
+            {
+                draw_line(data, a, b, 0Xff0000);
+                return(1);
+                break;
+            }
+        }
+        x++;
+    }
+    return(0);
+}
+
+
 void ray_colesion(t_data *data)
 {
     int y = 0;
     int x = 0;
     int px = 0;
     int py = 0;
-    double tpx =0;
-    double tpy = 0;
-    int a[2];
-	int b[2];
+    int up_down = 0;
     
     printf("pa = %f\n",data->pa );
     double degre = (data->pa / pi) * 180;
@@ -27,80 +68,28 @@ void ray_colesion(t_data *data)
         }
         y++;
     }
+    y = py;
     if((degre >= 0 && degre < 90) || (degre <= 360 && degre > 270))
     {
-        y = py;
+        up_down = 1;
         y--;
         while(y >= 0)
         {
-            x = 0;
-            while (data->result[y][x])
-            {
-                if(data->result[y][x] == '1') //wall detect posetion
-                {
-                    tpy = ((data->player_y) - (((y + 1) * 50))) + 7.5;
-                    tpx = tan(data->pa) * tpy;
-                    a[0] = data->player_x + 7.5;
-                    a[1] = data->player_y + 7.5;
-                    b[0] =   tpx + data->player_x;
-                    b[1] = (((y + 1) * 50));
-                    if(b[0] >= x * 50 && b[0] <= (x + 1) * 50)
-                    {
-                        draw_line(data, a, b, 0Xff0000);
-                        printf("x * 50 = %d , x + 1 * 50 = %d\n",x * 50,(x + 1) * 50);
-                        printf("x = %d\n px = %d \n",x,px);
-                        printf("b[0]x = %d\n y = %f \n",b[0],tpy);
-                        printf("tpx = %f\n",tpx);
-                        break;
-                    }
-                }
-                x++;
-            }
-            if(b[0] >= x * 50 && b[0] <= (x + 1) * 50)
+            if(y_detect(data,y,up_down) == 1)
                 break;
             y--;
         }
     }
     else
     {
-        y = py;
+        up_down = 0;
         y++;
         while(data->result[y])
         {
-            x = 0;
-            while (data->result[y][x])
-            {
-                if(data->result[y][x] == '1') //wall detect posetion
-                {
-                    tpy = ((((y) * 50)) - (data->player_y)) + 7.5;
-                    tpx = -tan(data->pa) * tpy;
-                    a[0] = data->player_x + 7.5;
-                    a[1] = data->player_y + 7.5;
-                    b[0] =   tpx + data->player_x;
-                    b[1] = (((y) * 50));
-                    if(b[0] >= x * 50 && b[0] <= (x + 1) * 50)
-                    {
-                        draw_line(data, a, b, 0Xff0000);
-                        printf("x * 50 = %d , x + 1 * 50 = %d\n",x * 50,(x + 1) * 50);
-                        printf("x = %d\n px = %d \n",x,px);
-                        printf("b[0]x = %d\n y = %f \n",b[0],tpy);
-                        printf("tpx = %f\n",tpx);
-                        break;
-                    }
-                }
-                x++;
-            }
-            if(b[0] >= x * 50 && b[0] <= (x + 1) * 50)
+            if(y_detect(data,y,up_down) == 1)
                 break;
             y++;
         }
     }
-    
-    // }
-    // if((data->pa >= (3*pi)/4 && data->pa <= pi) || (data->pa >= 0 && data->pa <= pi / 4)  )
-    // {
-    //     printf("hi\n");
-    // }
-    printf("===============\n\n");
     
 }
