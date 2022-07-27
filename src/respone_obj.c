59af3026d2944f57923b746acbc380cc9dfda5fe
mohammed <togachivon@gmail.com>
2022-07-27 20:59:02 +0100

    make_wall_and_earth

--- src/respone_obj.c
+++ src/respone_obj.c
@@ -7,25 +7,25 @@ void	respone_obj(t_data *data)
 
 	x = 0;
 	y = 0;
+	rander_image(&data->img, (t_rect){0, data->result_hight/2 * 50,data->result_with   * 50,   data->result_hight/2 * 50, 16777121});
+	rander_image(&data->img, (t_rect){0, 0 * 50,data->result_with   * 50,   data->result_hight/2 * 50, 56831});
 	while (data->result[y] != '\0')
 	{
 		while (data->result[y][x] != '\0')
 		{
 			if (data->result[y][x] == 49 )
-			{
 				rander_image(&data->img, (t_rect){x * 10, y * 10,10, 10, 9524926});
-			}
 			else if (data->result[y][x] == 48 || data->result[y][x] == 'N' || data->result[y][x] == 'S' ||
 					data->result[y][x] == 'W' || data->result[y][x] == 'E')
-				{
 					rander_image(&data->img, (t_rect){x * 10, y * 10,10, 10, 11796399});
-				}
-				
+			else
+				rander_image(&data->img, (t_rect){x * 10, y * 10,10, 10, 0});
 			x++;
 		}
 		x = 0;
 		y++;
 	}
+	
 	x = 0;
 	y = 0;
 	while (data->result[y] != '\0')
